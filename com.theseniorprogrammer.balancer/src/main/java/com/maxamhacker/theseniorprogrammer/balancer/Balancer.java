7de8c7961717dadd1b0ac8c179b770c2dea00a82
Maxam Hacker <maxam.hacker@gmail.com>
2018-08-23 01:21:58 +0300

    Started with Balancer

---
+++ com.theseniorprogrammer.balancer/src/main/java/com/maxamhacker/theseniorprogrammer/balancer/Balancer.java
@@ -0,0 +1,13 @@
+package com.maxamhacker.theseniorprogrammer.balancer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Balancer 
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(Balancer.class, args);
+    }
+}
---
+++ com.theseniorprogrammer.balancer/src/test/java/com/maxamhacker/theseniorprogrammer/balancer/BalancerTest.java
@@ -0,0 +1,38 @@
+package com.maxamhacker.theseniorprogrammer.balancer;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class BalancerTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public BalancerTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( BalancerTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
