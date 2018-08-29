23090283b1f2d8fa725cb581ee26704ea1e197a9
Maxam Hacker <maxam.hacker@gmail.com>
2018-08-29 14:30:13 +0300

    Continue investigation

--- theseniorprogrammer.site.offline/src/index.js
+++ theseniorprogrammer.site.offline/src/index.js
@@ -1,5 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+var config = {
+  content: [{
+    type: 'row',
+    content: [
+        {
+        type:'component',
+        componentName: 'example',
+        componentState: { text: 'Component 1' }
+        },
+      {
+        type:'component',
+        componentName: 'example',
+        componentState: { text: 'Component 2' }
+        },
+      {
+        type:'component',
+        componentName: 'example',
+        componentState: { text: 'Component 3' }
+        }
+    ]
+  }]
+};
 
-ReactDOM.render(<App />, document.getElementById('root'));
+var myLayout = new GoldenLayout( config );
+
+myLayout.registerComponent( 'example', function( container, state ){
+  container.getElement().html( '<h2>' + state.text + '</h2> \
+                                <div id="editor">public static void main() {}</div> \
+                                <script>\
+                                    var editor = ace.edit("editor");\
+                                    editor.setTheme("ace/theme/twilight"); \
+                                    var JavaMode = ace.require("ace/mode/java").Mode; \
+                                    editor.session.setMode(new JavaMode()); \
+                                </script>');
+});
+
+myLayout.init();
+
+
+
+
