55f269cbe1326be0fa9346d8afac640692837671
Maxam Hacker <maxam.hacker@gmail.com>
2019-09-03 21:56:01 +0300

    Theme and mode

--- com.theseniorprogrammer.pathsfinder/ui/electra/theraphosa.js
+++ com.theseniorprogrammer.pathsfinder/ui/electra/theraphosa.js
@@ -1,6 +1,8 @@
 const { remote } = require('electron');
 const phosa = require('../theraphosa/ace')
+const phosaTheme = require("../theraphosa/theme/monokai.js");
+const phosaMode = require("../theraphosa/mode/javascript.js").Mode;
 
 var phosaEditor = phosa.edit('editor');
-//phosaEditor.setTheme("../theraphosa/theme/monokai.js");
-//phosaEditor.getSession().setMode('../../theraphosa/mode/javascript.js');
+phosaEditor.setTheme(phosaTheme);
+phosaEditor.getSession().setMode(new phosaMode());
