1be946bfa6f46716137d4cd8f13caa19f29ea295
Maxam Hacker <maxam.hacker@gmail.com>
2019-09-04 23:09:21 +0300

    good idea )))

--- com.theseniorprogrammer.codehacker/ui/electra/components/browser.js
+++ com.theseniorprogrammer.codehacker/ui/electra/components/browser.js
@@ -1,31 +1,33 @@
 import React, {Component} from 'react'
 import PropTypes from 'prop-types'
+import fs from 'fs'
 
 class Browser extends Component {
 
   constructor(props) {
     super(props);
+
+    this.state = {
+      dir: fs.readdirSync('./')
+    };
+    
   }
 
   render() {
     return React.createElement(
+
       'ul', {},
 
-      this.props.project.map(path => {
-        return React.createElement('li', {}, path);
-      }),
+      this.state.dir.map(file => {
+        var stat = fs.statSync(file);
+        if (stat.isFile())
+          return React.createElement('li', {}, file);
+        if (stat.isDirectory())
+          return React.createElement('ul', {}, file);
+      })
 
-      React.createElement('li', {}, 'test0.txt'),
-      React.createElement('li', {}, 'test1.txt')
     );
   }
 }
-
-Browser.defaultProps = {
-  project: [
-    'test0.txt',
-    'test1.txt'
-  ]
-};
 
 export {Browser}
