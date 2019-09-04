fcf074912e79b7befa532779d00c42c6019d3e0d
Maxam Hacker <maxam.hacker@gmail.com>
2019-09-04 19:25:49 +0300

    test version

--- com.theseniorprogrammer.codehacker/ui/electra/components/browser.js
+++ com.theseniorprogrammer.codehacker/ui/electra/components/browser.js
@@ -10,10 +10,22 @@ class Browser extends Component {
   render() {
     return React.createElement(
       'ul', {},
+
+      this.props.project.map(path => {
+        return React.createElement('li', {}, path);
+      }),
+
       React.createElement('li', {}, 'test0.txt'),
       React.createElement('li', {}, 'test1.txt')
     );
   }
 }
+
+Browser.defaultProps = {
+  project: [
+    'test0.txt',
+    'test1.txt'
+  ]
+};
 
 export {Browser}
