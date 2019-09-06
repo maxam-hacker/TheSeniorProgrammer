cadeb6023ca9a5e03c31d55f2739ae51b08e4d74
Maxam Hacker <maxam.hacker@gmail.com>
2019-09-06 13:19:46 +0300

    Images for files and folders

--- com.theseniorprogrammer.codehacker/ui/electra/components/folder.js
+++ com.theseniorprogrammer.codehacker/ui/electra/components/folder.js
@@ -6,6 +6,9 @@ class FolderView extends Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+
+    };
   }
 
   onFolderClick(e) {
@@ -13,10 +16,28 @@ class FolderView extends Component {
   }
 
   render() {
-    return React.createElement('li', { onClick: this.onFolderClick.bind(this) }, this.props.name);
+    // https://woodpig07.github.io/react-nested-file-tree/
+    // http://tutorials.jenkov.com/svg/path-element.html
+    var htmlObj = {__html: 
+      '<svg ' +
+        'xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" ' +
+          'version="1.1" baseProfile="full" ' +
+          'width="24" height="24" ' +
+          'viewBox="0 0 24.00 24.00" ' +
+          'enable-background="new 0 0 24.00 24.00" ' +
+        'xml:space="preserve">' +
+        '<path d="M0 0h24v24H0z" fill="none"></path>' +
+        '<path d="M20 6h-8l-2-2H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2zm0 12H4V8h16v10z"></path>' +
+      '</svg>'
+    };
+    return React.createElement(
+      'li', { onClick: this.onFolderClick.bind(this) }, 
+          React.createElement('span', { className: 'svg', dangerouslySetInnerHTML: htmlObj }),
+          this.props.name);
   }
 
 }
+
 
 FolderView.propTypes = {
   name: PropTypes.string.isRequired,
