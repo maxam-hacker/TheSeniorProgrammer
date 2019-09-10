6f63e14906bd9090245f0799391a1f34e46c7243
Maxam Hacker <maxam.hacker@gmail.com>
2019-09-10 21:23:10 +0300

    New layout... and approach )))

--- com.theseniorprogrammer.codehacker/ui/electra/components/electra.js
+++ com.theseniorprogrammer.codehacker/ui/electra/components/electra.js
@@ -9,6 +9,22 @@ import PathMasterView from './pathmaster/view'
 class Electra extends Component {
 
     render() {
+        return React.createElement(SplitterLayout, { primaryIndex: 0, percentage: true, secondaryInitialSize: 80, vertical: true }, 
+            React.createElement(PathMasterView, { path: '.' }),
+            React.createElement(SplitterLayout, { primaryIndex: 0, percentage: true, secondaryInitialSize: 84}, 
+                    React.createElement(Browser, { path: './' }),
+                    React.createElement(SplitterLayout, { primaryIndex: 0, percentage: true, secondaryInitialSize: 60 },
+                        React.createElement(Theraphosa, { path: './', id: '_calls' }),
+                        React.createElement(SplitterLayout, { primaryIndex: 0, percentage: true, secondaryInitialSize: 66 },
+                            React.createElement(Browser, { path: './' }),
+                            React.createElement(Theraphosa, { path: './', id: '_methods' }),
+                        )
+                    )
+            )
+        )};
+
+    /*
+    render() {
         return React.createElement(SplitterLayout, { primaryIndex: 0, percentage: true, secondaryInitialSize: 80 }, 
             React.createElement(Browser, { path: './' }),
             React.createElement(SplitterLayout, { primaryIndex: 0, percentage: true, secondaryInitialSize: 80, vertical: true }, 
@@ -20,6 +36,7 @@ class Electra extends Component {
             )
         );
     }
+    */
 }
 
 export {Electra}
