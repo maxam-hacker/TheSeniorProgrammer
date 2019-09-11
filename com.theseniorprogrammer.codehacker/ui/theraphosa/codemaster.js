364adf04cff5e2f16e0edba8cbe04e585a412b82
Maxam Hacker <maxam.hacker@gmail.com>
2019-09-11 16:44:54 +0300

    The same...

--- com.theseniorprogrammer.codehacker/ui/theraphosa/codemaster.js
+++ com.theseniorprogrammer.codehacker/ui/theraphosa/codemaster.js
@@ -47,57 +47,7 @@ var callsExtractor = require("../electra/paths").getCallsForFile;
             var line = session.getLine(call.end.line);
             session.insert({ row: call.end.line, column: line.length + 1 }, "\n");
             
-            if (text == "\t")
-                text = editor.session.getTabString();
-    
-            // remove selected text
-            if (!editor.selection.isEmpty()) {
-                var range = editor.getSelectionRange();
-                cursor = editor.session.remove(range);
-                editor.clearSelection();
-            }
-            else if (editor.session.getOverwrite() && text.indexOf("\n") == -1) {
-                var range = new Range.fromPoints(cursor, cursor);
-                range.end.column += text.length;
-                editor.session.remove(range);
-            }
-    
-            if (text == "\n" || text == "\r\n") {
-                var line = session.getLine(cursor.row);
-                if (cursor.column > line.search(/\S|$/)) {
-                    var d = line.substr(cursor.column).search(/\S|$/);
-                    session.doc.removeInLine(cursor.row, cursor.column, cursor.column + d);
-                }
-            }
-            editor.clearSelection();
-    
-            var start = cursor.column;
-            var lineState = session.getState(cursor.row);
-            var line = session.getLine(cursor.row);
-            var shouldOutdent = mode.checkOutdent(lineState, line, text);
             session.insert(cursor, text);
-    
-            if (transform && transform.selection) {
-                if (transform.selection.length == 2) { // Transform relative to the current column
-                    editor.selection.setSelectionRange(
-                        new Range(cursor.row, start + transform.selection[0],
-                                  cursor.row, start + transform.selection[1]));
-                } else { // Transform relative to the current row.
-                    editor.selection.setSelectionRange(
-                        new Range(cursor.row + transform.selection[0],
-                                  transform.selection[1],
-                                  cursor.row + transform.selection[2],
-                                  transform.selection[3]));
-                }
-            }
-    
-            if (session.getDocument().isNewLine(text)) {
-                var lineIndent = mode.getNextLineIndent(lineState, line.slice(0, cursor.column), session.getTabString());
-    
-                session.insert({row: cursor.row+1, column: 0}, lineIndent);
-            }
-            if (shouldOutdent)
-                mode.autoOutdent(lineState, session, cursor.row);
         }
         
     }).call(CodeMaster.prototype);
