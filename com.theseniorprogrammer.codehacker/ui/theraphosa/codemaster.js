6d2f0da2786c362a688286177893e3626b10f999
Maxam Hacker <maxam.hacker@gmail.com>
2019-09-15 12:38:57 +0300

    The same approach for inheritance like in Ace

--- com.theseniorprogrammer.codehacker/ui/theraphosa/codemaster.js
+++ com.theseniorprogrammer.codehacker/ui/theraphosa/codemaster.js
@@ -3,55 +3,56 @@ define(function(require, exports, module) {
 
 var Editor = require("./editor").Editor;
 var callsExtractor = require("../electra/paths").getCallsForFile;
+var oop = require("./lib/oop")
+var event = require("./lib/event")
 
-    var CodeMaster = function(renderer, session, options) {
-        Editor.call(this, renderer, session, options);
-        this.currentFile = undefined;
-    };
+var CodeMaster = function(renderer, session, options) {
+    Editor.call(this, renderer, session, options);
+    this.currentFile = undefined;
+    var mouseTarget = this.renderer.getMouseEventTarget();
+    event.addListener(mouseTarget, 'click', this.onMouseClick.bind(this));
+};
+oop.inherits(CodeMaster, Editor);
 
-    CodeMaster.prototype = Object.create(Editor.prototype);
+(function() {
 
-    (function() {
+    this.onMouseClick = function(event) {
+        var editor = event.editor;
+        var cursor = this.getCursorPosition();
+        var currentRow = cursor.row;
+        var currentColumn = cursor.column;
+        var currentCall = undefined;
+        var calls = callsExtractor(this.currentFile);
 
-        this.addEventListener('click', event => {
-            var editor = event.editor;
-            var cursor = editor.getCursorPosition();
-            var currentRow = cursor.row;
-            var currentColumn = cursor.column;
-            var currentCall = undefined;
-            var calls = callsExtractor(editor.currentFile);
-
-            if (calls !== undefined) 
-                calls.forEach(call => {
-                    if (currentRow >= call.start.line && 
-                        currentRow <= call.end.line &&
-                        currentColumn >= call.start.column && 
-                        currentColumn <= call.end.column)
-                        currentCall = call;
-                });
+        if (calls !== undefined) 
+            calls.forEach(call => {
+                if (currentRow >= call.start.line && 
+                    currentRow <= call.end.line &&
+                    currentColumn >= call.start.column && 
+                    currentColumn <= call.end.column)
+                    currentCall = call;
+            });
 
             if (currentCall !== undefined)
-                this.expandCall(editor, currentCall);
-        });
+                this.expandCall(currentCall);
+    };
 
-        this.setCurrentFile = function(filename) {
-            this.currentFile = filename;
-        };
+    this.setCurrentFile = function(filename) {
+        this.currentFile = filename;
+    };
 
-        this.expandCall = function(editor, call) {
-            var text = call.path.method.text;
-            var session = editor.session;
-            var mode = session.getMode();
-            var cursor = { row: call.end.line + 1, column: call.end.column + 1};
+    this.expandCall = function(call) {
+        var text = call.path.method.text;
+        var session = this.session;
+        var cursor = { row: call.end.line + 1, column: call.end.column + 1};
 
-            var line = session.getLine(call.end.line);
-            session.insert({ row: call.end.line, column: line.length + 1 }, "\n");
+        var line = session.getLine(call.end.line);
+        session.insert({ row: call.end.line, column: line.length + 1 }, "\n");
             
-            session.insert(cursor, text);
-        }
+        session.insert(cursor, text);
+    }
         
-    }).call(CodeMaster.prototype);
+}).call(CodeMaster.prototype);
 
-    exports.CodeMaster = CodeMaster;
-
+exports.CodeMaster = CodeMaster;
 }); 
