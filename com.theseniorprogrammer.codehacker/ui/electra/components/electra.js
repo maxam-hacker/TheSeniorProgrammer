e49f717db5f3bebfba4c55ca2c46697985dd9882
Maxam Hacker <maxam.hacker@gmail.com>
2019-09-21 22:00:19 +0300

    Correct root directory

--- com.theseniorprogrammer.codehacker/ui/electra/components/electra.js
+++ com.theseniorprogrammer.codehacker/ui/electra/components/electra.js
@@ -10,14 +10,14 @@ class Electra extends Component {
 
     render() {
         return React.createElement(SplitterLayout, { primaryIndex: 0, percentage: true, secondaryInitialSize: 80, vertical: true }, 
-            React.createElement(PathMasterView, { path: '/Users/a.kiselev/Documents/GitLab/TheSeniorProgrammer/com.theseniorprogrammer.codehacker/ui' }),
+            React.createElement(PathMasterView, { path: '.' }),
             React.createElement(SplitterLayout, { primaryIndex: 0, percentage: true, secondaryInitialSize: 84}, 
-                    React.createElement(Browser, { path: '/Users/a.kiselev/Documents/GitLab/TheSeniorProgrammer/com.theseniorprogrammer.codehacker/ui/', type: 'calls' }),
+                    React.createElement(Browser, { path: './', type: 'calls' }),
                     React.createElement(SplitterLayout, { primaryIndex: 0, percentage: true, secondaryInitialSize: 60 },
-                        React.createElement(Theraphosa, { path: '/Users/a.kiselev/Documents/GitLab/TheSeniorProgrammer/com.theseniorprogrammer.codehacker/ui/', id: '_calls', type: 'calls' }),
+                        React.createElement(Theraphosa, { path: './', id: '_calls', type: 'calls' }),
                         React.createElement(SplitterLayout, { primaryIndex: 0, percentage: true, secondaryInitialSize: 66 },
-                            React.createElement(Browser, { path: '/Users/a.kiselev/Documents/GitLab/TheSeniorProgrammer/com.theseniorprogrammer.codehacker/ui/', type: 'methods' }),
-                            React.createElement(Theraphosa, { path: '/Users/a.kiselev/Documents/GitLab/TheSeniorProgrammer/com.theseniorprogrammer.codehacker/ui/', id: '_methods', type: 'methods' }),
+                            React.createElement(Browser, { path: './', type: 'methods' }),
+                            React.createElement(Theraphosa, { path: './', id: '_methods', type: 'methods' }),
                         )
                     )
             )
