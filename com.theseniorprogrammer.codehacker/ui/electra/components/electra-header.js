1b0601d2046b6aaeb4d12f19c41621892b6ef2d1
Maxam Hacker <maxam.hacker@gmail.com>
2019-10-14 16:21:43 +0300

    Titles!!!

--- com.theseniorprogrammer.codehacker/ui/electra/components/electra-header.js
+++ com.theseniorprogrammer.codehacker/ui/electra/components/electra-header.js
@@ -4,7 +4,7 @@ class ElectraHeader extends Component {
 
     render() {
 
-        this.logoPathCircle = React.createElement('path', { className: 'logo-circle', 
+        this.logoPath = React.createElement('path', { className: 'logo-lines', 
         d: 
         'M45,3 \
         A27,27 0 0,0 45,57 \
@@ -16,12 +16,16 @@ class ElectraHeader extends Component {
         L45, 40 \
         A47,25 0 0,1 45,3 \
         '});
-
-        this.logoSvg = React.createElement('svg', {}, this.logoPathCircle);
-
+        this.logoSvg = React.createElement('svg', {}, this.logoPath);
         this.logoWrapper = React.createElement('div', {className: 'logo-wrapper'}, this.logoSvg);
 
-        this.theHeader = React.createElement('div', {className: 'electra-header'}, this.logoWrapper);
+        this.titleSp = React.createElement('span', {className: 'title-senior-programmer'}, 'TheSeniorProgrammer');
+        this.titleCh = React.createElement('span', {className: 'title-code-hacker'}, 'CodeHacker :');
+        this.titlePr = React.createElement('span', {className: 'title-project-name'}, 'project-name');
+        this.titleChWrapper = React.createElement('div', {className: 'title-project-wrapper'}, this.titleCh, this.titlePr);
+        this.titleWrapper = React.createElement('div', {className: 'title-wrapper'}, this.titleSp, this.titleChWrapper);
+
+        this.theHeader = React.createElement('div', {className: 'electra-header'}, this.logoWrapper, this.titleWrapper);
 
         return this.theHeader;
     }
