0ef289575e6c0026deff5866dc98ea56d7e10c62
Maxam Hacker <maxam.hacker@gmail.com>
2019-10-14 15:19:16 +0300

    Logo!!! )))

--- com.theseniorprogrammer.codehacker/ui/electra/components/electra-header.js
+++ com.theseniorprogrammer.codehacker/ui/electra/components/electra-header.js
@@ -4,7 +4,24 @@ class ElectraHeader extends Component {
 
     render() {
 
-        this.theHeader = React.createElement('div', {className: 'electra-header'});
+        this.logoPathCircle = React.createElement('path', { className: 'logo-circle', 
+        d: 
+        'M45,3 \
+        A27,27 0 0,0 45,57 \
+        M45,3 \
+        A27,27 0 0,1 45,57 \
+        M45,3 \
+        L45, 17 \
+        A47,25 0 0,1 45,57 \
+        L45, 40 \
+        A47,25 0 0,1 45,3 \
+        '});
+
+        this.logoSvg = React.createElement('svg', {}, this.logoPathCircle);
+
+        this.logoWrapper = React.createElement('div', {className: 'logo-wrapper'}, this.logoSvg);
+
+        this.theHeader = React.createElement('div', {className: 'electra-header'}, this.logoWrapper);
 
         return this.theHeader;
     }
