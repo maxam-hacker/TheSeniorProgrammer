21bb12c5125378009ae3cafffbcd0a8b4f19e408
Maxam Hacker <maxam.hacker@gmail.com>
2019-10-15 17:33:27 +0300

    Code refactoring

--- com.theseniorprogrammer.codehacker/ui/electra/components/electra.js
+++ com.theseniorprogrammer.codehacker/ui/electra/components/electra.js
@@ -16,38 +16,6 @@ import {VerticalSplitter} from './vsplitter'
 
 class Electra extends Component {
 
-    /*
-    render() {
-        return React.createElement(SplitterLayout, { primaryIndex: 0, percentage: true, secondaryInitialSize: 80, vertical: true }, 
-            React.createElement(PathMasterView, { path: '.' }),
-            React.createElement(SplitterLayout, { primaryIndex: 0, percentage: true, secondaryInitialSize: 84}, 
-                    React.createElement(Browser, { path: './', type: 'calls' }),
-                    React.createElement(SplitterLayout, { primaryIndex: 0, percentage: true, secondaryInitialSize: 60 },
-                        React.createElement(Theraphosa, { path: './', id: '_calls', type: 'calls' }),
-                        React.createElement(SplitterLayout, { primaryIndex: 0, percentage: true, secondaryInitialSize: 66 },
-                            React.createElement(Browser, { path: './', type: 'methods' }),
-                            React.createElement(Theraphosa, { path: './', id: '_methods', type: 'methods' }),
-                        )
-                    )
-            )
-        )};
-    */
-
-    /*
-    render() {
-        return React.createElement('div', { className: 'electra' }, 
-                    React.createElement('div', { className: 'electra-header' }), 
-                    React.createElement('div', { className: 'electra-content' },
-                        React.createElement(VerticalSplitter, {},
-                                    React.createElement(Browser, { path: './', type: 'calls' }),
-                                    React.createElement(Theraphosa, { path: './', id: '_calls', type: 'calls' })
-                        )
-                    ),
-                    React.createElement('div', { className: 'electra-footer' })
-        );
-    }
-    */
-
     render() {
         this.electraHeader = React.createElement(ElectraHeader, {});
         this.electraLeftMenu = React.createElement(ElectraLeftMenu, {});
