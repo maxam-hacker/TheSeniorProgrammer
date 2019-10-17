2371c749f60aad2aa46221839b3c9706aa9ad02a
Maxam Hacker <maxam.hacker@gmail.com>
2019-10-17 18:53:37 +0300

    Need a grid

--- com.theseniorprogrammer.codehacker/ui/electra/components/seniorprogrammer/hacks.js
+++ com.theseniorprogrammer.codehacker/ui/electra/components/seniorprogrammer/hacks.js
@@ -5,9 +5,25 @@ class MagazHacks extends Component {
 
     render() {
 
-        this.page = React.createElement('div', {className: 'magaz-hacks'});
+        this.springHack0 = React.createElement('div', {className: 'magaz-hacks-spring'});
+        this.springHack1 = React.createElement('div', {className: 'magaz-hacks-spring'});
+        this.springHack2 = React.createElement('div', {className: 'magaz-hacks-spring'});
+        this.springHack3 = React.createElement('div', {className: 'magaz-hacks-spring'});
+        this.springHack4 = React.createElement('div', {className: 'magaz-hacks-spring'});
+        this.springHack5 = React.createElement('div', {className: 'magaz-hacks-spring'});
 
-        return this.page;
+        this.line0 = React.createElement('div', {className: 'magaz-hacks-page-line-0'},
+                                                this.springHack0,
+                                                this.springHack1,
+                                                this.springHack2,);
+
+        this.pageWrapper = React.createElement('div', {className: 'magaz-pages'}, 
+                                this.line0,
+                                this.springHack3,
+                                this.springHack4,
+                                this.springHack5);
+
+        return this.pageWrapper;
     }
 }
 
