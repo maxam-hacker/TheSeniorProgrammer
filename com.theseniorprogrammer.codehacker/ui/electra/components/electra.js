9ddaf9e5e975320123e3333e2796d79db2f9fccb
Maxam Hacker <maxam.hacker@gmail.com>
2019-10-17 18:16:45 +0300

    General layout for site

--- com.theseniorprogrammer.codehacker/ui/electra/components/electra.js
+++ com.theseniorprogrammer.codehacker/ui/electra/components/electra.js
@@ -4,24 +4,28 @@ import {ElectraLeftMenu} from './electra-left-menu';
 import {ElectraRightMenu} from './electra-right-menu';
 import {ElectraContent} from './electra-content';
 import {ElectraFooter} from './electra-footer';
-
+import {Magaz} from './seniorprogrammer/magaz'
 
 class Electra extends Component {
 
     render() {
+
         this.electraHeader = React.createElement(ElectraHeader, {});
         this.electraLeftMenu = React.createElement(ElectraLeftMenu, {});
         this.electraRightMenu = React.createElement(ElectraRightMenu, {});
         this.electraContent = React.createElement(ElectraContent, {});
         this.electraFooter = React.createElement(ElectraFooter, {});
-
-        return React.createElement('div', { className: 'electra' },
+        this.codeHacker = React.createElement('div', { className: 'electra' },
                     this.electraHeader,
                     this.electraLeftMenu,
                     this.electraContent,
                     this.electraRightMenu,
                     this.electraFooter
         );
+
+        this.Magaz = React.createElement(Magaz, {className: 'magaz'});
+
+        return this.Magaz;
     }
 }
 
---
+++ com.theseniorprogrammer.codehacker/ui/electra/components/seniorprogrammer/hacks.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+
+class MagazHacks extends Component {
+
+    render() {
+
+        this.page = React.createElement('div', {className: 'magaz-hacks'});
+
+        return this.page;
+    }
+}
+
+export {MagazHacks}
