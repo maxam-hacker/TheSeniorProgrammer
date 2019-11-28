58abd241745d90f7925793560be3e112bf1ea167
Maxam Hacker <maxam.hacker@gmail.com>
2019-11-28 20:10:30 +0300

    Refactoring method parameters

--- com.theseniorprogrammer.codehacker/ui/electra/components/seniorprogrammer/backender.js
+++ com.theseniorprogrammer.codehacker/ui/electra/components/seniorprogrammer/backender.js
@@ -2,39 +2,30 @@ import axios from 'axios';
 
 class BackendHandler {
 
-    BackendHandler() {
+    constructor() {
         this.seniorBaseURL = 'http://localhost:17070/';
         this.commandTopicGroups = '/groups';
-        this.commandTopicContent = 'http://localhost:17070/topic?';
-        this.commandOpenCodeHacker = 'http://localhost:17070/codehacker?';
-
-        this.debug = true;
-
-        this.getTopicGroups = this.getTopicGroups.bind(this);
+        this.commandTopicContent = '/topic';
+        this.commandOpenCodeHacker = '/codehacker';
     }
 
     getTopicGroups(callback, onerror) {
-
-        console.log(this.seniorBaseURL);
         
         axios
             .request({
                 method: "get",
-                url: '/groups',
+                url: this.commandTopicGroups,
                 crossDomain: true,
-                baseURL: 'http://localhost:17070/',
+                baseURL: this.seniorBaseURL,
             })
             .then(function(response) {
-
                 console.log(response.data);
-
                 if (callback)
                     callback(response.data);
             })
             .catch(function (error) {
                 console.log('BackendHandler :: getTopicGroups :: error');
                 console.log(error);
-
                 if (onerror)
                     onerror(error);
             });
@@ -42,21 +33,24 @@ class BackendHandler {
 
     getTopicContent(topicName, callback, onerror) {
 
-        axios.get(this.commandTopicContent + topicName)
-
+        axios
+            .request({
+                method: "get",
+                url: this.commandTopicContent,
+                crossDomain: true,
+                baseURL: this.seniorBaseURL,
+                params: {
+                    topic: topicName
+                },
+            })
             .then(function(response) {
-
-                if (this.debug)
-                    console.log(response.data);
-
+                console.log(response.data);
                 if (callback)
                     callback(response.data);
             })
-
             .catch(function (error) {
                 console.log('BackendHandler :: getTopicContent :: error');
                 console.log(error);
-
                 if (onerror)
                     onerror(error);
             });
